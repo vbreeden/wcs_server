b062fc88a23ea4f345609d95e0717cc698879541
Virgil Breeden <virgil.breeden@gtri.gatech.edu>
2017-09-27 12:16:06 -0400

    Dance On Time model implemented and migrations performed.

--- danceontime/models/danceontime.py
+++ danceontime/models/danceontime.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.core.validators import MinValueValidator
 
-# Create your models here.
+
+class DanceOnTime(models.Model):
+    dance_name = models.CharField(max_length=64)
+    time_signature = models.CharField(max_length=5)
+    tempo = models.PositiveIntegerField(validators=[MinValueValidator(50)])
+    ticks_mp3 = models.CharField(max_length=128)
+    vocals_mp3 = models.CharField(max_length=128)
